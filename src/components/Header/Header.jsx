d41c5cd31e6472b7bada45abee942f7ac2902f22
andrecosta8 <andrecosta8@gmail.com>
2022-11-25 16:52:01 +0000

    add basci header with navigate and a basic footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Header.css'
 
 const Header = () => {
+  let navigate = useNavigate();
   return (
-    <div>Header</div>
+    <div className='headerDesign'>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/")},250)}>Home</div>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/meals")},250)}>Meals</div>
+            <div className="linkDesign" onClick={()=>setTimeout(()=>{navigate("/addmeal")},250)}>Add Meal</div>
+         </div>
   )
 }
 
