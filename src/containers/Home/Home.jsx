2f215a092d2ce880d39407b45707708005f1885d
Ricardo Rodriguez <ricardo.rodriguez@nb-rrodriguez.home>
2022-11-29 12:24:35 +0100

    Search input created

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  const [user, setUser] = useState([]);
-  const [choose, setChoosen] = useState([]);
-  const [search, setSearch] = useState("");
-
   return <div>Home</div>;
 };
 
